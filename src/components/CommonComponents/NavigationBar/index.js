037f30330316cbc7f16a4ddc81a826b8c68ccc37
lfal7838 <lashan.faliq@syscolabs.com>
2020-10-01 11:44:33 +0530

    Add login modal

--- src/components/CommonComponents/NavigationBar/index.js
+++ src/components/CommonComponents/NavigationBar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Collapse,
@@ -12,47 +12,56 @@ import {
 import './styles.scss';
 import formatMessages from 'components/formatMessages';
 import Icon from 'components/Shared/Icon';
+import LoginModal from 'components/Shared/Modal';
 import messages from './messages';
 
 const NavigationBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoginModalVisible, setIsLoginModalVisible] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
+  const onClickLogin = () => {
+    setIsLoginModalVisible(true);
+  };
 
   return (
-    <Navbar light expand="md">
-      <Link to="/">
-        avoplan
-      </Link>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <Button outline>
-              <span>
-                {formatMessages(messages.areYouALawyer)}
-              </span>
-            </Button>
-          </NavItem>
-          <NavItem>
-            <Button color="link ">
-              <Icon name="question-circle" />
-              <span>
-                {formatMessages(messages.needHelp)}
-              </span>
-            </Button>
-          </NavItem>
-          <NavItem>
-            <Button color="link ">
-              <Icon name="user-circle" />
-              <span>
-                {formatMessages(messages.login)}
-              </span>
-            </Button>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <>
+      <Navbar light expand="md">
+        <Link to="/">
+          avoplan
+        </Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Button outline>
+                <span>
+                  {formatMessages(messages.areYouALawyer)}
+                </span>
+              </Button>
+            </NavItem>
+            <NavItem>
+              <Button color="link ">
+                <Icon name="question-circle" />
+                <span>
+                  {formatMessages(messages.needHelp)}
+                </span>
+              </Button>
+            </NavItem>
+            <NavItem>
+              <Button color="link" onClick={onClickLogin}>
+                <Icon name="user-circle" />
+                <span>
+                  {formatMessages(messages.login)}
+                </span>
+              </Button>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      {isLoginModalVisible && <LoginModal title="title" isOpen={isLoginModalVisible} onClosed={() => { setIsLoginModalVisible(false); }} onSubmit={() => { }}><div>tst</div></LoginModal>}
+    </>
+
   );
 };
 
---
+++ src/components/Shared/Modal/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import {
+  Modal, ModalHeader, ModalBody, ModalFooter, Button,
+} from 'reactstrap';
+import {
+  func, string, node, bool,
+} from 'prop-types';
+
+const ModalComponent = (props) => {
+  const {
+    className,
+    title,
+    children,
+    onSubmit,
+    isOpen,
+    ...rest
+  } = props;
+
+  const [isVisible, setIsModalVisible] = useState(isOpen);
+
+  const toggle = () => setIsModalVisible(!isVisible);
+  const onClickSubmit = () => {
+    onSubmit();
+    setIsModalVisible(false);
+  };
+  const onClickCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Modal isOpen={isVisible} className={className} toggle={toggle} {...rest}>
+      <ModalHeader>{title}</ModalHeader>
+      <ModalBody>
+        {children}
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={onClickSubmit}>Do Something</Button>
+        <Button color="secondary" onClick={onClickCancel}>Cancel</Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+ModalComponent.propTypes = {
+  className: string,
+  title: string.isRequired,
+  children: node.isRequired,
+  onSubmit: func.isRequired,
+  centered: bool,
+  isOpen: bool,
+};
+
+ModalComponent.defaultProps = {
+  className: '',
+  centered: true,
+  isOpen: false,
+};
+
+export default ModalComponent;
