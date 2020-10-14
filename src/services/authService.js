fee854db99a458bbc5cf3b7e7c8e8048cd185242
lfal7838 <lashan.faliq@syscolabs.com>
2020-10-14 18:09:57 +0530

    Add a api helper

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import { post } from 'utils/apiHelper';
 
-export const serviceUrl = 'http://localhost:8000';
-
-export const loginUserService = (values) => {
+// eslint-disable-next-line import/prefer-default-export
+export const loginUserService = async (values) => {
   const { email, password } = values;
 
-  axios.post(`${serviceUrl}/auth/login`, {
+  const { result, error } = await post('/auth/login', {
     email,
     password,
   });
+  console.log(result, error);
 };
