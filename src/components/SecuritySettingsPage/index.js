0a020f00918cf86f815f92aacd756c2127ea564d
Fathimath Hanau <hanau.xd@gmail.com>
2021-06-19 03:49:46 +0530

    feature(security): add info box and change pw box
    Please enter the commit message for your changes. Lines starting

--- src/components/SecuritySettingsPage/index.js
+++ src/components/SecuritySettingsPage/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import formatMessages from 'components/formatMessages';
 import ProAccountContainer from 'components/Shared/ProAccountContainer';
+import ChangePassword from './components/ChangePassword';
+import InformationBox from './components/InformationBox';
+import { Container, Title, Pan } from './styles';
+import messages from './messages';
 
 function SecuritySettingsPage() {
-  return <ProAccountContainer>Security Settings</ProAccountContainer>;
+  return (
+    <ProAccountContainer>
+      <Title>{formatMessages(messages.title)}</Title>
+      <Container>
+        <Pan>
+          <InformationBox />
+          <ChangePassword />
+        </Pan>
+      </Container>
+    </ProAccountContainer>
+  );
 }
 
 export default SecuritySettingsPage;
